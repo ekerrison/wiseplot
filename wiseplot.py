bf4124a37412b0a0b81a4043f132e8338a464e25
Emily Kerrison <efkerrison@gmail.com>
2023-05-22 09:31:04 +1000

    converting code to be python 3 compliant

--- wiseplot.py
+++ wiseplot.py
@@ -8,13 +8,13 @@ import os
 import sys
 from math import *
 from numpy import *
-from pylab import *
+import matplotlib.pyplot as plt
 from matplotlib import rc
-rc('text', usetex=True)
+#rc('text', usetex=True)
+#rc('font',**{'family':'STIXGeneral', 'size':18})
 rc('font',**{'family':'serif','serif':['serif'],'size':20})
 from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage,AnnotationBbox
 from matplotlib.cbook import get_sample_data
-from matplotlib._png import read_png
 from astropy.io import ascii
 
 ##################################################
@@ -43,7 +43,7 @@ werr1,werr2,werr3 = ['w1sigmpro','w2sigmpro','w3sigmpro']
 
 # Options
 ploterr = True # Toggle for plotting errors
-print 'Plotting of errors is set to... %s' % ploterr
+print('Plotting of errors is set to... %s' % ploterr)
 
 # For labelling figure output
 if ploterr == True:
@@ -54,32 +54,32 @@ else:
 # Read file
 try: 
     filename = sys.argv[1]
-    print 'Reading from file... %s' % filename
+    print('Reading from file... %s' % filename)
 except:
-    print 'No filename specified! Using example...'
+    print('No filename specified! Using example...')
     filename = 'example.txt'
 fname = '.'.join(filename.split('.')[0:-1])
 
 # Read data
 d = ascii.read(filename)
-print
-print 'Number of sources:',len(d)
-print
+print()
+print('Number of sources:',len(d))
+print()
 
 # Check for user-defined column headings:
 try:
-    print 'Data:',wise1,wise2,wise3
+    print('Data:',wise1,wise2,wise3)
     if ploterr == True:
-        print 'Errors:',werr1,werr2,werr3
-    print '... specified column headings detected!'
+        print('Errors:',werr1,werr2,werr3)
+    print('... specified column headings detected!')
 
 except:
-    print 'You have not defined column headings, so I will assume the defaults...'
+    print('You have not defined column headings, so I will assume the defaults...')
     wise1,wise2,wise3 = ['W1','W2','W3']
-    print 'Data:',wise1,wise2,wise3
+    print('Data:',wise1,wise2,wise3)
     if ploterr == True:
         werr1,werr2,werr3 = ['W1err','W2err','W3err']    
-        print 'Errors:',werr1,werr2,werr3
+        print('Errors:',werr1,werr2,werr3)
 
 # Create columns
 try:
@@ -87,7 +87,7 @@ try:
     w2 = d[wise2]
     w3 = d[wise3]
 except:
-    print 'Fatal error: could not find specified column headings... quitting!'
+    print('Fatal error: could not find specified column headings... quitting!')
     sys.exit()
 
 # Errors
@@ -101,12 +101,12 @@ if ploterr == True:
 # WISE background plot (Wright et al 2010)
 ## DO NOT CHANGE!
 print
-print 'Setting up plot...'
+print('Setting up plot...')
 ratio = 1.065
 xdim = 8
 ydim = xdim*ratio
-figure(figsize=(xdim,ydim))
-ax = gca()
+plt.figure(figsize=(xdim,ydim))
+ax = plt.gca()
 
 # Redo x-labels
 xticks = arange(-1,8,1)
@@ -127,12 +127,12 @@ ax.set_yticks(ytix)
 ax.set_yticklabels(ylab)
 
 # Set the axis limits
-xlim(0,1)
-ylim(0,1)
+plt.xlim(0,1)
+plt.ylim(0,1)
 
 # Incorporate the background image
 sfig = 'wright2010data.png'
-arr_lena = read_png(sfig)
+arr_lena = plt.imread(sfig)
 imagebox = OffsetImage(arr_lena, zoom=0.27)
 ab = AnnotationBbox(imagebox, [0.5,0.5],
                     xybox=(0., 0.),
@@ -144,12 +144,12 @@ ax.add_artist(ab)
 ##################################################
 
 # Plot
-print 'Plotting data...'
+print('Plotting data...')
 (x,y) = wise2pix(w2-w3,w1-w2)
 
 # Scatter plot
 # Change plotting parameters of data here if needed
-scatter(x,y,facecolor='r',marker='o',s=10,zorder=101,alpha=1.0,linewidth=0.5)
+plt.scatter(x,y,facecolor='r',marker='o',s=10,zorder=101,alpha=1.0,linewidth=0.5)
 
 # Error plot
 if ploterr == True:
@@ -158,22 +158,22 @@ if ploterr == True:
     (xe,ye) = wise2pix((w2-w3)+w23e,(w1-w2)+w12e)
     xe1 = xe-x
     ye1 = ye-y
-    errorbar(x,y,xerr=xe1,yerr=ye1,fmt=None,zorder=100,ecolor='k')
+    plt.errorbar(x,y,xerr=xe1,yerr=ye1,fmt='None',zorder=100,ecolor='k')
 
 # Labels
-xlabel('[ 4.6 ] - [ 12 ] in mag')
-ylabel('[ 3.4 ] - [ 4.6 ] in mag')
+plt.xlabel('[ 4.6 ] - [ 12 ] in mag')
+plt.ylabel('[ 3.4 ] - [ 4.6 ] in mag')
 
 # Text credit 
 # DO NOT REMOVE!
-text(0.995,1.01,'Image credit: Chao-Wei Tsai; from Wright et al. 2010',fontsize=8,horizontalalignment='right')
+plt.text(0.995,1.01,'Image credit: Chao-Wei Tsai; from Wright et al. 2010',fontsize=8,horizontalalignment='right')
 
 ##################################################
 
 # NOTE: DISPLAYED FIGURE IS SCALED WRONG
 # SAVED PDF IS SCALED CORRECTLY!
-print 'Saving figure...'
-savefig('%s_%sallwise.pdf' % (fname,perr),dpi=200,bbox_inches='tight')
-print
-print '... done!'
+print('Saving figure...')
+plt.savefig('%s_%sallwise.pdf' % (fname,perr),dpi=200,bbox_inches='tight')
+print()
+print('... done!')
 
